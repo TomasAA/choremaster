39d3f3156a2253569d10134c184b2749db8a6562
Scorpiion <Robert@dartvoid.com>
2014-05-04 00:35:41 +0200

    Save

--- client/web/lib/models/task.dart
+++ client/web/lib/models/task.dart
@@ -2,5 +2,26 @@ part of models;
 
 class TaskModel {
   String name;
+  String type;  // nfs or manual
+  bool state;
+  int points;
+  
+  TaskModel();
+
+  TaskModel.fromJson(Map json) {
+    name = json["name"];
+    type = json["type"];
+    state = json["state"];
+    points = json["points"];
+  }
+  
+  Map toJson() {
+    return {
+      "name": name,
+      "type": type,
+      "state": state,
+      "points": points
+    };
+  }
 }
 
--- server/resources/tasks.dart
+++ server/resources/tasks.dart
@@ -5,19 +5,132 @@ import '../../client/web/lib/models/models.dart';
 class Tasks extends Vane {
   // Get state for all tasks
   Future getAll() {
-    return close("All tasks");
+    print("Inside getAll tasks");
+
+    // Get a mongodb variable so that we can access the database
+    mongodb.then((mongodb) {
+      // Create a collection variable so we can access a specific collection of
+      // data from the database
+      var tasksColl = mongodb.collection("tasks");
+
+      // Find all data that is in the collection "users", make it to a list
+      tasksColl.find().toList().then((List<Map> tasks) {
+        var taskObjects = new List<TaskModel>();
+
+        // Create a list of activity objects (to get rid of mongodb's _id)
+        tasks.forEach((task) {
+          taskObjects.add(new TaskModel.fromJson(task));
+        });
+
+        // Close sends the activityObjects list and closes the response
+        close(taskObjects);
+      }).catchError((e) {
+        // If there was an error, return a empty list
+        close([]);
+      });
+    }).catchError((e) {
+      // If there was an error, return a empty list
+      close([]);
+    });
+
+    return end;
   }
-  
+
+  // Get task 'task'
+  Future get() {
+    // Get name value from url, /tasks/$name
+    var name = path[1];
+
+    print("Inside get task");
+
+    // Get a mongodb variable so that we can access the database
+    mongodb.then((mongodb) {
+      // Create a collection variable so we can access a specific collection of
+      // data from the database
+      var tasksColl = mongodb.collection("tasks");
+
+      // Find all data that is in the collection "users", make it to a list
+      tasksColl.findOne({
+        "name": name
+      }).then((Map task) {
+        // Create a new user object (to get rid of mongodb's _id)
+        var taskObject = new TaskModel.fromJson(task);
+
+        // Close sends the user object and closes the response
+        close(taskObject.toJson());
+      }).catchError((e) {
+        // If there was an error, return a empty map
+        close({});
+      });
+    }).catchError((e) {
+      // If there was an error, return a empty map
+      close({});
+    });
+
+    return end;
+  }
+
+  // Create a new task 'task'
+  Future create() {
+    var task = new TaskModel();
+
+    // Get name value from url, /tasks/$name/$type/$points
+    task.name = path[1];
+
+    // Get name type from url, /tasks/$name/$type/$points
+    task.type = path[2];
+
+    // Get name points from url, /tasks/$name/$type/$points
+    task.points = path[3];
+
+    print("Inside create task");
+
+    // Get a mongodb variable so that we can access the database
+    mongodb.then((mongodb) {
+      // Create a collection variable so we can access a specific collection of
+      // data from the database
+      var tasksColl = mongodb.collection("tasks");
+
+      // Check that an activity with the same name don't already exists
+      tasksColl.findOne({
+        "name": task.name
+      }).then((Map existingTask) {
+        // If we did not find any existing task with the same name, then
+        // create the new task
+        if (existingTask == null) {
+          // Save new user to database
+          tasksColl.insert(task.toJson()).then((dbRes) {
+            log.info("Mongodb: ${dbRes}");
+
+            close("ok");
+          }).catchError((e) {
+            log.warning("Unable to insert new task: ${e}");
+            close("error");
+          });
+        } else {
+          close("Error, user already exists");
+        }
+      }).catchError((e) {
+        log.warning("Unable to search for existing task: ${e}");
+        close("error");
+      });
+    }).catchError((e) {
+      log.warning("Unable to insert new task: ${e}");
+      close("error");
+    });
+
+    return end;
+  }
+
   Future setState() {
     // Set/update state for one task
-    
+
     return close("Setting state");
   }
-  
+
   Future getState() {
     // Get state for one task
-    
+
     return close("Returning state for one task");
   }
 }
-
